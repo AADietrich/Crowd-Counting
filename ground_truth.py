28c319fe70bbe5e04d60c50f8dc38961f922726f
Aaron DIetrich <aarondietrich1066@gmail.com>
2023-03-31 10:33:40 -0700

    Added ground_truth, additional blocks in loading More functionality for heatmap generation

---
+++ ground_truth.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL.Image as pil
+import sklearn.neighbors as neighbors
+
+
+##Coef for Gaussian sample distance, determined experimentally in MCNN paper (Zhang, etal)
+GAUSSIAN_BETA = 0.3
+##Number nearest samples, TBD
+K = 5
+
+def findRadius(points):
+    averages = []
+    nbrs = neighbors.NearestNeighbors(n_neighbors = K).fit(points)
+    distances, indices = nbrs.kneighbors(points)
+    for d in distances:
+        averages.append(np.average(np.asarray(d[1:])))
+    return np.asarray(averages)
